01cbf59e46e975f66dc943299c3b47a028391a28
Jeremy Burman <jburman@gmail.com>
2017-12-15 16:03:12 -0500

    Updated package version

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("W8lessLabs.Owin.ServiceWorker")]
-[assembly: AssemblyDescription("OWIN middleware to easily enable Service Workers on ASP.NET applications. Based on Mads Kristensen's ASP.NET Core PWA package https://github.com/madskristensen/WebEssentials.AspNetCore.ServiceWorker")]
+[assembly: AssemblyDescription("OWIN middleware to easily enable Service Workers on ASP.NET applications.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("W8lessLabs")]
 [assembly: AssemblyProduct("W8lessLabs.Owin.ServiceWorker")]
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ba3af3e5-ecb5-4930-af0f-14d5402c1a64")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
